6f489802015329fbccb5abb4748bf84499a07ac5
Vadymshairan <95377527+Vadymshairan@users.noreply.github.com>
2022-08-13 02:51:50 +0300

    Задание 1 -  добавляет библиотеку SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
 console.log(galleryItems);
+
+// 1. Создание и рендер разметки
+const galleryEl = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryEl.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}" 
+        alt="${description}"/>
+      </a>`;
+    })
+    .join('');
+}
+let lightbox = new SimpleLightbox('.gallery a', {
+  // captions: true,
+  // captionType: 'attr',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
