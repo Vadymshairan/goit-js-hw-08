d3624da1775fed5631e0fe36d269517e6fcf932a
Vadymshairan <95377527+Vadymshairan@users.noreply.github.com>
2022-08-25 22:33:29 +0300

    +

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,16 +3,17 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
 player.on(
   'timeupdate',
   throttle(({ seconds }) => {
-    localStorage.setItem('videoplayer-current-time', `${seconds}`);
+    localStorage.setItem(LOCALSTORAGE_KEY, `${seconds}`);
   }, 1000)
 );
 
 player
-  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY))
   .then(function (seconds) {})
   .catch(function (error) {
     switch (error.name) {
